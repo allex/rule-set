8dc9216ea74a6cb80eca209a0c188960a2b4e8fa
Allex Wang <allex.wxn@gmail.com>
2021-10-13 16:06:04 +0800

    feat: add condition operators: include, test, not

--- src/RuleSet.ts
+++ src/RuleSet.ts
@@ -1,4 +1,19 @@
-export type RuleMatcher = (s: string) => boolean;
+/**
+ * A conditions composer, based on Tobias Koppers RuleSet
+ */
+
+// Supports condition types:
+// -------------------------
+// <condition>: /regExp/
+// <condition>: function(arg) {}
+// <condition>: "starting"
+// <condition>: [<condition>] // or
+// <condition>: { and: [<condition>] }
+// <condition>: { not: [<condition>] }
+// <condition>: { test: <condition>, include: <condition>, exclude: <condition> }
+// <condition>: { whitelist: RuleList, blacklist: RuleList }
+
+export type RuleMatcher = (s: any) => boolean;
 
 const notMatcher = (matcher: RuleMatcher): RuleMatcher => {
   return s => !matcher(s)
@@ -31,7 +46,8 @@ export const normalizeCondition = (condition?: any): RuleMatcher => {
   if (typeof condition === 'function') {
     return condition
   }
-  if (condition instanceof RegExp) {
+  // RegExp-Like interface (with **test()**)
+  if (typeof condition === 'object' && typeof condition.test === 'function') {
     return condition.test.bind(condition)
   }
   if (Array.isArray(condition)) {
@@ -49,17 +65,36 @@ export const normalizeCondition = (condition?: any): RuleMatcher => {
   Object.keys(condition).forEach(key => {
     const v = condition[key]
     switch (key) {
+      case 'include':
+      case 'test':
+      case 'whitelist':
+        if (v) matchers.push(normalizeCondition(v))
+        break;
+      case 'and':
+        if (v) {
+          const items = v.map((c: any) => normalizeCondition(c))
+          matchers.push(andMatcher(items))
+        }
+        break;
+      case 'not':
       case 'exclude':
       case 'blacklist':
-        matchers.push(notMatcher(v))
-        break
-      case 'whitelist':
-        matchers.push(v)
+        if (v) {
+          matchers.push(notMatcher(normalizeCondition(v)))
+        }
         break
       default:
         throw new Error(`Unexcepted property ${key} in condition`)
     }
   })
+
+  if (matchers.length === 0) {
+    throw new Error('Excepted condition but got ' + condition)
+  }
+
+  if (matchers.length === 1) {
+    return matchers[0]
+  }
 
   return andMatcher(matchers)
 }
